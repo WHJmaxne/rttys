5b4f375aaef8a3275dd37b0a6eb169d09d460af3
whj <whjmaxne@outlook.com>
2022-09-15 14:12:42 +0800

    none

--- ui/src/views/Login.vue
+++ ui/src/views/Login.vue
@@ -2,43 +2,38 @@
   <Card :title="signup ? $t('Sign up') : $t('Authorization Required')" class="login-container">
     <Form ref="login" :model="formData" :rules="ruleValidate" :label-width="100" label-position="left">
       <FormItem :label="$t('Username')" prop="username">
-        <Input v-model="formData.username" prefix="ios-person-outline" :placeholder="$t('Enter username...')"
-          @on-enter="handleSubmit" />
+        <Input v-model="formData.username" prefix="ios-person-outline" :placeholder="$t('Enter username...')" @on-enter="handleSubmit"/>
       </FormItem>
       <FormItem :label="$t('Password')" prop="password">
-        <Input type="password" v-model="formData.password" prefix="ios-lock-outline"
-          :placeholder="$t('Enter password...')" password @on-enter="handleSubmit" />
+        <Input type="password" v-model="formData.password" prefix="ios-lock-outline" :placeholder="$t('Enter password...')" password @on-enter="handleSubmit"/>
       </FormItem>
       <FormItem>
-        <Button type="primary" size="large" long @click="handleSubmit">{{ signup ? $t('Sign up') : $t('Sign in')
-        }}</Button>
+        <Button type="primary" size="large" long @click="handleSubmit">{{ signup ? $t('Sign up') : $t('Sign in') }}</Button>
       </FormItem>
     </Form>
-    <p v-if="signup" style="text-align: center">{{ $t('Already have an account?') }}<a href="/login"
-        style="text-decoration: none; color: #1c7cd6">{{ $t('Sign in') }}</a></p>
-    <p v-else style="text-align: center">{{ $t('New to Rttys?') }}<a href="/login?signup=1"
-        style="text-decoration: none; color: #1c7cd6">{{ $t('Sign up') }}</a></p>
+    <p v-if="signup" style="text-align: center">{{ $t('Already have an account?') }}<a href="/login" style="text-decoration: none; color: #1c7cd6">{{ $t('Sign in') }}</a></p>
+    <p v-else style="text-align: center">{{ $t('New to Rttys?') }}<a href="/login?signup=1" style="text-decoration: none; color: #1c7cd6">{{ $t('Sign up') }}</a></p>
   </Card>
 </template>
 
 <script>
 export default {
   name: 'Login',
-  data () {
+  data() {
     return {
       signup: false,
-      formData: {
+        formData: {
         username: '',
         password: ''
       },
       ruleValidate: {
-        username: [{ required: true, trigger: 'blur', message: this.$t('username is required') }],
-        password: [{ required: true, trigger: 'blur', message: this.$t('password is required') }]
+        username: [{required: true, trigger: 'blur', message: this.$t('username is required')}],
+        password: [{required: true, trigger: 'blur', message: this.$t('password is required')}]
       }
     }
   },
   methods: {
-    handleSubmit () {
+    handleSubmit() {
       (this.$refs['login']).validate(valid => {
         if (valid) {
           const params = {
@@ -67,7 +62,7 @@ export default {
       });
     }
   },
-  created () {
+  created() {
     this.signup = this.$route.query.signup === '1';
     sessionStorage.removeItem('rttys-sid');
   }
@@ -75,11 +70,11 @@ export default {
 </script>
 
 <style scoped>
-.login-container {
-  width: 500px;
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-}
+  .login-container {
+    width: 500px;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+  }
 </style>
